a0f28a5246ff21e3c8d5794324caab4c8bfbfd71
Yeung69420 <97737189+tommy750714@users.noreply.github.com>
2022-05-20 20:48:34 +0800

    Delete src directory

--- src/common/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL:'https://BscComBlogSample.chengka.repl.co/api/v1',
-  headers:{
-    'Content-type': 'application/json'
-  }
-})
--- src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Articles from './Articles';
-
-function Home(){
-  return (<Articles/>);
-}
-export default Home;
